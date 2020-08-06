cf69d6ceed45c5c31e8efeaafd4b2f11bd602b1c
unknown <akansha584@gmail.com>
2020-08-07 01:07:20 +0530

    first commit

--- src/store/reducer/dataReducer.js
+++ src/store/reducer/dataReducer.js
@@ -1,18 +1,16 @@
-import { userData } from "../test";
-
 const initialState = {
-    userData: [],
-}
-    export const dataReducer = (state = initialState , action) => {
-    switch(action.type) {
-        case "FETCH_USER_DATA" :
-            // var userName=[];
-            // action.data.map(data => userName.push(data.real_name))
-            return{
-                ...state,              
-                userData: action.data
-            }
-        default:
-            return state;
-    }
-}
+  userData: [],
+};
+export const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_USER_DATA":
+      // var userName=[];
+      // action.data.map(data => userName.push(data.real_name))
+      return {
+        ...state,
+        userData: action.data,
+      };
+    default:
+      return state;
+  }
+};
